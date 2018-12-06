ac024dcd6cc4f44f2091774daa4ef2e25c30a7a5
CB_ATL_110_David_Williams <cb_atl_110_david_williams@MacBook-Air-87.local>
2018-12-06 10:59:51 -0500

    set up local database and heroku deployment?

--- tunr_react/settings.py
+++ tunr_react/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
+    'rest_framework'
+
+
 ]
 
 MIDDLEWARE = [
@@ -75,8 +79,11 @@ WSGI_APPLICATION = 'tunr_react.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tunr_react_WDI18',
+        'USER': 'tunr_admin',
+        'PASSWORD': 'tunr_admin',
+        'HOST': 'localhost'
     }
 }
 
@@ -118,3 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
